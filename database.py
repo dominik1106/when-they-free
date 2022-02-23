d0adf64d288856d06b5ca09c8ad8b8095a43bfd2
dominik1106 <dominik.p.s@gmx.de>
2022-02-23 21:29:04 +0100

    added .env file

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'
+from dotenv import load_dotenv
+load_dotenv()
+
+
+SQLALCHEMY_DATABASE_URL = os.environ.get('SQL_URI')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
