b64508e64b6da0ad27a19f80e744c34f6b9d0240
dominik1106 <dominik.p.s@gmx.de>
2022-02-23 16:55:52 +0100

    Added Pydantic ORM schemas

---
+++ schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    email: str
+    mongo_id: bytes
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    hsh_password: str
+
+    class Config:
+        orm_mode = True
