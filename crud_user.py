809db6642e9300fe8eac4fe7054dff87c64347e3
dominik1106 <dominik.p.s@gmx.de>
2022-02-23 23:09:07 +0100

    replaced the mongo_id through a uuid4 generated with the uuid module

--- crud_user.py
+++ crud_user.py
@@ -1,19 +1,20 @@
 from sqlalchemy.orm import Session
 import models, schemas
-
+from uuid import uuid4
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
-def get_user_by_mongoid(db: Session, mongo_id: bytes):
-    return db.query(models.User).filter(models.User.mongo_id == mongo_id).first()
+def get_user_by_uuid(db: Session, uuid: str):
+    return db.query(models.User).filter(models.User.uuid == uuid).first()
 
 def create_user(db: Session, user: schemas.UserCreate):
     #TODO: Password hash implementation
     hsh_pwd = user.password + 'Bcrypt Hash'
-    db_user = models.User(email=user.email, mongo_id=user.mongo_id, hashed_password=hsh_pwd)
+    uuid_4 = uuid4().hex
+    db_user = models.User(email=user.email, uuid=uuid_4, hashed_password=hsh_pwd)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
--- schemas.py
+++ schemas.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 
 class UserBase(BaseModel):
     email: str
-    mongo_id: str
 
 class UserCreate(UserBase):
     password: str
 
 class User(UserBase):
     id: int
-    hsh_password: str
+    hashed_password: str
+    uuid: str
 
     class Config:
         orm_mode = True
